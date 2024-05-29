50b759270e85b0bcf554c276eedd7799926779d7
Jinwoooooooo <jinwooya55@naver.com>
2024-05-29 15:42:46 +0900

    0529

--- btree.py
+++ btree.py
@@ -90,11 +90,55 @@ class BTree:
     #------------------------------------
     # 완전 이진 트리 여부를 확인하여 True, False로 반환한다.
     def isComplete(self):
-        pass
+        queue = Queue()  # 레벨 순서대로 저장할 queue 생성.
+        queue.add(self)  # root node부터 queue에 저장.
+        danger = False   # 불완전 트리 가능성 발견하면 True로 설정, 이후의 조사에서 참조.
+
+        # queue에서 다음 방문할 노드를 차례대로 꺼내서 조사한다.
+        while not queue.isEmpty():       # 방문할 노드가 남아 있으면
+            currentNode = queue.remove() # 다음 조사할 노드를 꺼낸다.
+            # 왼쪽 자식 노드 상항 처리.
+            if currentNode.left:    # 왼쪽 자식 노드가 있고,
+                if danger:          # 이전에 위험한 상황(무자식 상황)이 있었으면
+                    return False    # 불완전 트리로 결론.
+                queue.add(currentNode.left) # 다음에 방문하기 위해 queue에 저장.
+            else:
+                danger = True
+
+            # 오른쪽 자식 노드 상황 처리
+            if currentNode.right:
+                if danger:
+                    return False
+                queue.add(currentNode.right)
+            else:
+                danger = True
+
+        # while문 종료
+        return True # 더 방문할 노드 없이 while 종료되면 완전이진트리 결론.
+ 
     #------------------------------------
     # 완전 이진 트리를 배열(list)로 변환하기 (list를 반환, 단, 리스트의 첫 항목은 None)
+    # 완전 이진 트리가 아닌 경우에는 적절한 오류 메세지 출력과 함께 None를 반환.
     def toList(self):
-        pass
+
+        if self.isComplete():
+            # list로 변환하는 코드 작성.
+            lst = [None]
+            queue = Queue()  # level Order 방문을 위한 queue 생성.
+            queue.add(self)  
+
+            while not queue.isEmpty():
+                currentNode = queue.remove()
+                lst.append(currentNode.key)
+                if currentNode.left:
+                    queue.add(currentNode.left)
+                if currentNode.right:
+                    queue.add(currentNode.right)
+            # 최종 리스트 반환
+            return lst
+        else:
+            print("toList: Not Complete Binary Tree.")
+            return None
 
 
     #------------------------------------
@@ -107,7 +151,23 @@ tE = BTree("E")
 tF = BTree("F")
 tG = BTree("G")
 tH = BTree("H")
+tI = BTree("I")
+tJ = BTree("J")
 #---------------------
+tA.left = tB
+tA.right = tC
+tB.left = tD
+tB.right = tE
+tD.left = tI
+tD.right = tG
+tE.left = tH
+tC.right = tF
+tC.left = tJ
+print(tA.toList())
+
+
+#---------------------
+"""
 tA.left = tB
 tA.right = tC
 tB.left = tD
@@ -120,6 +180,7 @@ treeList = tA.toList()
 
 print("Tree A의 높이는 %d입니다."%(tA.height()))
 print("Tree B의 높이는 %d입니다."%(tB.height()))
+"""
 
 
 
